3d26ab8e68030e971c55b20159b9c82395162ffb
Firede <firede@firede.us>
2013-04-24 21:56:26 +0800

    remove controller 'users'

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
 var express = require('express'),
   routes = require('./routes'),
-  user = require('./routes/user'),
   http = require('http'),
   path = require('path');
 
@@ -28,7 +27,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
